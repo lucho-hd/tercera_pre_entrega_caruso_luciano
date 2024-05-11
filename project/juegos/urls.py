090b8e7fd8805e557170a9ca4ca971c16dba9389
Luciano <lucho200211@gmail.com>
2024-05-11 20:56:21 -0300

    Add a new route 'juegos/detalles'

--- project/juegos/urls.py
+++ project/juegos/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from juegos.views import (
-    lista_juegos
+    lista_juegos,
+    detalle_juego
 )
 
 app_name = "juegos"
 
 urlpatterns = [
-    path("lista_juegos", lista_juegos, name="lista_juegos")
+    path("lista_juegos", lista_juegos, name="lista_juegos"),
+    path("detalle/<int:id>", detalle_juego, name="detalle_juego" ),
 ]
--- project/juegos/views.py
+++ project/juegos/views.py
@@ -4,6 +4,13 @@ from juegos.models import Juego
 # Create your views here.
 
 def lista_juegos(req):
+    """ Trae la lista de juegos desde la Base de Datos """
     query   = Juego.objects.all()
     context = {"juegos": query} 
     return render(req, "juegos/lista_juegos.html", context)
+
+def detalle_juego(req, id: int):
+    """ Muestra la información de un juego en específico mediante su 'id: int' """
+    query   = Juego.objects.get(id=id)
+    context = {"juego":query} 
+    return render(req, "juegos/detalle_juego.html", context)
