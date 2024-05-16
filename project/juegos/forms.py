457dff73c5b6f40578c878b5699b68b02c896c86
Luciano <lucho200211@gmail.com>
2024-05-16 19:16:10 -0300

    modified: juegos/forms.py

--- project/juegos/forms.py
+++ project/juegos/forms.py
@@ -4,10 +4,11 @@ from . import models
 class JuegosForm(forms.ModelForm):
     class Meta:
         model = models.Juego
-        fields = ["titulo", "descripcion", "precio", "imagen", "plataformas", "desarrolladoras", "generos", "editores"]
+        fields = ["titulo", "descripcion", "fecha_lanzamiento", "precio", "imagen", "plataformas", "desarrolladoras", "generos", "editores"]
         widgets = {
             'titulo': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Título del Juego'}),
             'descripcion': forms.Textarea(attrs={'class': 'form-control', 'rows': 6, 'placeholder': 'Descripción del Juego'}),
+            'fecha_lanzamiento': forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Ingrese la fecha de lanzamiento'}),
             'precio': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Precio del Juego'}),
             'imagen': forms.FileInput(attrs={'class': 'form-control', 'placeholder': 'Seleccione una imagen'}),
             'plataformas': forms.CheckboxSelectMultiple(attrs={'class': 'form-check-input'}),
@@ -18,6 +19,7 @@ class JuegosForm(forms.ModelForm):
         labels = {
             'titulo': 'Título',
             'descripcion': 'Descripción',
+            'fecha_lanzamiento': 'Fecha de Lanzamiento',
             'precio': 'Precio',
             'imagen': 'Imagen',
             'plataformas': 'Plataformas',
