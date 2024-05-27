66b8b87d51a82147d9e30e6db46052004c029609
Luciano <lucho200211@gmail.com>
2024-05-27 20:45:45 -0300

    modified: modal-eliminar.html and add screenshots of the site

--- project/config/settings.py
+++ project/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,6 +124,7 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media/' 
 
 LOGOUT_REDIRECT_URL = ''
+LOGIN_URL = reverse_lazy("core:iniciar-sesion")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
